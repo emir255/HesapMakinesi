5fa39add0f83ae6ee339b4c99a7f009783b28e5f
Emirhan Uzunget <emirhanuzunget25@gmail.com>
2023-03-04 20:02:43 +0300

    Proje dosyası ekle.

---
+++ HesapMakinesi/Form1.cs
@@ -0,0 +1,303 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HesapMakinesi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double x, y;
+        string yazi,islem;
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            label1.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "1";
+            }
+            else
+            {
+                textBox1.Text += "1";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "2";
+            }
+            else
+            {
+                textBox1.Text += "2";
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "3";
+            }
+            else
+            {
+                textBox1.Text += "3";
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "4";
+            }
+            else
+            {
+                textBox1.Text += "4";
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "5";
+            }
+            else
+            {
+                textBox1.Text += "5";
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "6";
+            }
+            else
+            {
+                textBox1.Text += "6";
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "7";
+            }
+            else
+            {
+                textBox1.Text += "7";
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "8";
+            }
+            else
+            {
+                textBox1.Text += "8";
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "9";
+            }
+            else
+            {
+                textBox1.Text += "9";
+            }
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            islem = "+";
+            if (label1.Text == "")
+            {
+                label1.Text = "0";
+                yazi = label1.Text;
+                y = Convert.ToDouble(yazi);
+                label1.Text = Convert.ToString(y + Convert.ToDouble(textBox1.Text));
+                label1.Text = label1.Text + "+";
+
+            }
+            else if (label1.Text != "0")
+            {
+                yazi = label1.Text.Remove(label1.Text.Length - 1, 1);
+                y = Convert.ToDouble(yazi);
+                label1.Text = Convert.ToString(y + Convert.ToDouble(textBox1.Text));
+                label1.Text = label1.Text + "+";
+            }
+            
+            
+
+
+            textBox1.Text = "0";
+
+
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            islem = "-";
+            if (label1.Text == "")
+            {
+                label1.Text = "0";
+                yazi = label1.Text;
+                y = Convert.ToDouble(yazi);
+                label1.Text = textBox1.Text;
+                label1.Text = label1.Text + "-";
+
+            }
+            else if (label1.Text != "0")
+            {
+                yazi = label1.Text.Remove(label1.Text.Length - 1, 1);
+                y = Convert.ToDouble(yazi);
+                label1.Text = Convert.ToString(y - Convert.ToDouble(textBox1.Text));
+                label1.Text = label1.Text + "-";
+            }
+
+
+
+
+            textBox1.Text = "0";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            islem = "*";
+            if (label1.Text == "")
+            {
+                label1.Text = "1";
+                yazi = label1.Text;
+                y = Convert.ToDouble(yazi);
+                label1.Text = Convert.ToString(y * Convert.ToDouble(textBox1.Text));
+                label1.Text = label1.Text + "*";
+
+            }
+            else if (label1.Text != "0")
+            {
+                yazi = label1.Text.Remove(label1.Text.Length - 1, 1);
+                y = Convert.ToDouble(yazi);
+                label1.Text = Convert.ToString(y * Convert.ToDouble(textBox1.Text));
+                label1.Text = label1.Text + "*";
+            }
+
+
+
+
+            textBox1.Text = "0";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            islem = "/";
+            if (label1.Text == "")
+            {
+                
+                
+                
+                label1.Text = Convert.ToString(Convert.ToDouble(textBox1.Text));
+                label1.Text = label1.Text + "/";
+
+            }
+            else if (label1.Text != "0")
+            {
+                yazi = label1.Text.Remove(label1.Text.Length - 1, 1);
+                y = Convert.ToDouble(yazi);
+                label1.Text = Convert.ToString(y / Convert.ToDouble(textBox1.Text));
+                label1.Text = label1.Text + "/";
+            }
+
+
+
+
+            textBox1.Text = "0";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            if (islem == "+")
+            {
+                yazi = label1.Text.Remove(label1.Text.Length - 1, 1);
+                y = Convert.ToDouble(yazi);
+                label1.Text += textBox1.Text + "=";
+                textBox1.Text = Convert.ToString(y + Convert.ToDouble(textBox1.Text));
+                
+            }
+            else if (islem == "-")
+            {
+                yazi = label1.Text.Remove(label1.Text.Length - 1, 1);
+                y = Convert.ToDouble(yazi);
+                label1.Text += textBox1.Text + "=";
+                textBox1.Text = Convert.ToString(y - Convert.ToDouble(textBox1.Text));
+            }
+            else if (islem == "*")
+            {
+                yazi = label1.Text.Remove(label1.Text.Length - 1, 1);
+                y = Convert.ToDouble(yazi);
+                label1.Text += textBox1.Text + "=";
+                textBox1.Text = Convert.ToString(y * Convert.ToDouble(textBox1.Text));
+            }
+            else if (islem == "/")
+            {
+                yazi = label1.Text.Remove(label1.Text.Length - 1, 1);
+                y = Convert.ToDouble(yazi);
+                label1.Text += textBox1.Text + "="; 
+                textBox1.Text = Convert.ToString(y / Convert.ToDouble(textBox1.Text));
+                
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "0")
+            {
+                textBox1.Text = "0";
+            }
+            else
+            {
+                textBox1.Text += "0";
+            }
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Length > 0)
+            {
+                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1, 1);
+
+                if (textBox1.Text.Length == 0)
+                {
+                    textBox1.Text = "0";
+                }
+            }
+        }
+    }
+}
